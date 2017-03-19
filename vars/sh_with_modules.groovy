87e122236877076d9aedf32c0da6ea8e70a8405a
Oleg Nenashev <o.v.nenashev@gmail.com>
2017-03-19 22:57:08 +0100

    Prevent double slash in the modules path

--- vars/sh_with_modules.groovy
+++ vars/sh_with_modules.groovy
@@ -7,7 +7,7 @@
 def call(Map params = [:]) {
     def modulesToLoad = params.containsKey('modules') ? params.modules : []
     def command = params.containsKey('command') ? params.command : "echo 'Command is not defined'"
-    def modulesPath = params.containsKey('modulesPath') ? params.modulesPath : "/usr/share/modules/"
+    def modulesPath = params.containsKey('modulesPath') ? params.modulesPath : "/usr/share/modules"
     
     String toInvoke = command
     for (int i = 0; i < modulesToLoad.size(); ++i) {
