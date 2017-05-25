6826043ed54e838d4c1bc9afa0359e43566e84da
Stefan Wallentowitz <stefan@wallentowitz.de>
2017-05-25 23:11:07 +0200

    Update lcci.groovy

--- vars/lcci.groovy
+++ vars/lcci.groovy
@@ -1,5 +1,14 @@
 #!/usr/bin/env groovy
 
-def sh(command) {
-  echo "${command}"
+class acme implements Serializable {
+    private String name
+    def setName(value) {
+        name = value
+    }
+    def getName() {
+        name
+    }
+    def sh(message) {
+      echo "${message}"
+    }
 }
