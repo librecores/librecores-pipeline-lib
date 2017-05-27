fbeb47449926ea34e6dc8191ad69f2a947be780c
Stefan Wallentowitz <stefan@wallentowitz.de>
2017-05-27 12:24:12 +0200

    Update Modules.groovy

--- src/org/librecores/ci/Modules.groovy
+++ src/org/librecores/ci/Modules.groovy
@@ -2,7 +2,10 @@ package org.librecores.ci
 class Modules implements Serializable {
   def steps
   Modules(steps) {this.steps = steps}
-  def load(args) {
-    steps.echo "${args}"
+  
+  String[] modules = []
+  
+  def load(String[] mods) {
+     modules += mods
   }
 }
