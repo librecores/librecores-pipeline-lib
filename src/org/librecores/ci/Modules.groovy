a5319ab8d41281db5b073e78bd8870284936c81d
Stefan Wallentowitz <stefan@wallentowitz.de>
2017-05-27 13:54:58 +0200

    Update Modules.groovy

--- src/org/librecores/ci/Modules.groovy
+++ src/org/librecores/ci/Modules.groovy
@@ -8,10 +8,7 @@ class Modules implements Serializable {
   def load(String[] modules) {
      for (int i = 0; i < modules.size(); ++i) {
         String module = modules[i]
-        def status = steps.sh(
-          script: "source /usr/share/modules/init/bash && module load ${module}",
-          returnStdout: true
-        ).trim()
+        steps.sh returnStdout: true, script: "source /usr/share/modules/init/bash && module load ${module}"
        //steps.echo "${status}"
      }
   }
