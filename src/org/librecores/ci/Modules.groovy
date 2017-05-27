669425d2f539fac95c36cadaa57fbb506b03698f
Stefan Wallentowitz <stefan@wallentowitz.de>
2017-05-27 15:54:35 +0200

    Update Modules.groovy

--- src/org/librecores/ci/Modules.groovy
+++ src/org/librecores/ci/Modules.groovy
@@ -10,8 +10,8 @@ class Modules implements Serializable {
         modules = [modules]
     }
     modules.each {
-       steps.sh "source /usr/share/modules/init/bash && module avail ${it} >> output"
-       def VERSION = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
+       steps.sh "source /usr/share/modules/init/bash && module avail ${it} 2> output"
+       def VERSION = steps.sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
       steps.echo "${VERSION}"
        //steps.echo "${status}"
        //modulesToLoad << module
