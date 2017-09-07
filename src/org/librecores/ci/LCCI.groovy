0087b1d069df5bbde557dd215e4b4ebced8c7fb4
Oleg Nenashev <o.v.nenashev@gmail.com>
2017-09-08 00:03:17 +0100

    Add LCCI class, which allows intelligent sources checkout on the development instance

---
+++ src/org/librecores/ci/LCCI.groovy
@@ -0,0 +1,28 @@
+package org.librecores.ci
+
+class LCCI {
+
+    def steps
+
+    LCCI(steps) {
+        this.steps = steps
+    }
+
+    @NonCPS
+    Boolean isRunningOnProduction() {
+        return steps.env.JENKINS_URL == 'https://ci.librecores.org/'
+    }
+
+    @NonCPS
+    void checkoutScmOrFallback(String fallback = null) {
+        if (isRunningOnProduction()) {
+            steps.checkout steps.scm
+        } else if (fallback == null) {
+            steps.fail "Cannot checkout SCM. Running on a non-production instance with undefined fallback"
+        }
+
+        steps.echo "Checking out from a fallback"
+        steps.git fallback
+    }
+
+}
