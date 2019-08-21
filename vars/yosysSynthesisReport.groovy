5bf96124f696415cb31e309e74b90dc4456f526b
Nancy-Chauhan <nancychn1@gmail.com>
2019-08-22 01:13:41 +0530

    Plot resource usage and cell stats

--- vars/yosysSynthesisReport.groovy
+++ vars/yosysSynthesisReport.groovy
@@ -10,5 +10,19 @@ def call(String core, String targetName, String yosysLogPath) {
         }
 
         shell "/test-scripts/extract-yosys-stats.py < \"${yosysLogPath}\""
+
+        plotGraph 'yosys-stats.csv', 'Resource Usage'
+        plotGraph 'yosys-cell-stats.csv', 'Cell Count'
     }
+}
+
+def plotGraph(csvSource, title) {
+    plot csvFileName: "plot-${csvSource}", csvSeries: [[file: csvSource, url: '']],
+            exclZero: true,
+            group: title,
+            numBuilds: '15',
+            style: 'line',
+            title: title,
+            useDescr: true,
+            yaxis: 'Values'
 }
